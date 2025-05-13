350e738570b5dd676ee54f825b6365cabb8c4d00
Archie Atkinson <archieatkinson97@gmail.com>
2025-05-13 22:32:12 +0100

    enables proper nvida drivers

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -46,4 +46,24 @@
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+    
+  hardware.graphics = {
+    enable = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+        sync.enable = true;
+    		intelBusId = "PCI:0:2:0";
+    		nvidiaBusId = "PCI:1:0:0";
+    	};
+  };
 }

648939316a6316bf6b595808f929e212ae57be59
Archie Atkinson <hello@archieatkinson.com>
2026-03-30 14:18:49 +0100

    Add hashed password

--- modules/common.nix
+++ modules/common.nix
@@ -65,6 +65,7 @@ in
   users.groups.plugdev = { };
   users.users.archie = {
     isNormalUser = true;
+    hashedPassword = "$y$j9T$SN1/W4HroOxr3kt8ADvg50$N.J8jOpSbzFqO6.9u/Kp1Z166u87emCJi89WeaY6c8D";
     description = "Archie";
     extraGroups = [
       "networkmanager"
